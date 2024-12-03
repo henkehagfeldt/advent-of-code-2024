d4a9cceee419da74f2833b7ef073f2863b2b1fd8
Henrik Hagfeldt <henke9606@hotmail.com>
2024-12-03 16:37:44 +0100

    Cleanup day 3

--- day3.py
+++ day3.py
@@ -4,15 +4,13 @@ from utils.files import getPath
 def part1():
 
     data = ""
-
     with open(getPath(3, example=False), "r") as f:
         data = f.readline()
 
     pattern = "mul\((\d{1,3}),(\d{1,3})\)"
-    matches = re.findall(pattern, data)
-
     result = 0
-    for match in matches:
+    
+    for match in re.findall(pattern, data):
         result += int(match[0]) * int(match[1])
 
     print(result)
@@ -24,13 +22,10 @@ def part2():
         data = f.readline()
 
     pattern = "mul\((\d{1,3}),(\d{1,3})\)|(do\(\))|(don't\(\))"
-    matches = re.findall(pattern, data)
-
     enabled = True
     result = 0
-    for match in matches:
-        print(match)
 
+    for match in re.findall(pattern, data):
         if match[2] != '':
             enabled = True
         elif match[3] != '':

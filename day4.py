1540d2aeb71be1de1efbd165b7aac603ec110eec
Henrik Hagfeldt <henke9606@hotmail.com>
2024-12-04 19:16:21 +0100

    Day 4 done

--- day4.py
+++ day4.py
@@ -1,10 +1,14 @@
-import re
 from utils.files import getPath
+
+xmas = "XMAS"
+all_directions = [(-1, -1), (0, -1), (1, -1),(-1, 0),(1, 0),(-1, 1),(0, 1),(1, 1)]
 
 def part1():
 
-    with open(getPath(4, example=True), "r") as f:
-        word_search = f.readlines()
+    word_search = []
+    with open(getPath(4, example=False), "r") as f:
+        for line in f.readlines():
+            word_search.append(line.strip())
 
     result = 0
     for y in range(len(word_search)):
@@ -12,57 +16,62 @@ def part1():
             value = word_search[y][x]
 
             if value == "X":
-                if(checkXmas(word_search, x, y)):
-                    print(f"Found XMAS at {x}, {y}")
-                    result += 1
+                for dir in all_directions:
+                    if findLetter(word_search, 1, x, y, dir[0], dir[1]):
+                        result += 1
     print(result)
 
-def checkXmas(map, x, y):
-    print(f"Checking for XMAS at {x}, {y}")
-    next_x, next_y = x, y
-    for letter in "MAS":
-        next_x, next_y = findLetterAround(map, letter, next_x, next_y)
-        if next_x == None or next_y == None:
-            return False
+def findLetter(map, li, x, y, xd, yd):
 
-def findLetterAround(map, letter, center_x, center_y):
+    if li == 4:
+        return True
+
+    if x + xd < 0 or x + xd >= len(map[0]):
+        return False
     
-    start_y = max(center_y - 1, 0)
-    end_y = min(center_y + 1, len(map) - 1)
-
-    start_x = max(center_x - 1, 0)
-    end_x = min(center_x + 1, len(map[0]) - 1)
-
-    print(f"Search Area: x {start_x}, {end_x} - y {start_y}, {end_y}")
-
-    for y in range(start_y, end_y + 1):
-        for x in range(start_x, end_x + 1):
-            print(f"Checking {x}, {y} for {letter}")
-            value = map[y][x]
-            if value == letter:
-                return x, y
+    if y + yd < 0 or y + yd >= len(map):
+        return False
     
-    return None, None
+    if map[y+yd][x+xd] == xmas[li]:
+        return findLetter(map, li + 1, x+xd, y+yd, xd, yd)
+    
+    return False
 
 def part2():
-    data = ""
+    word_search = []
+    with open(getPath(4, example=False), "r") as f:
+        for line in f.readlines():
+            word_search.append(line.strip())
 
-    with open(getPath(3, example=True), "r") as f:
-        data = f.readline()
-
-    pattern = "mul\((\d{1,3}),(\d{1,3})\)|(do\(\))|(don't\(\))"
-    enabled = True
     result = 0
+    for y in range(len(word_search)):
+        for x in range(len(word_search[y])):
+            value = word_search[y][x]
 
-    for match in re.findall(pattern, data):
-        if match[2] != '':
-            enabled = True
-        elif match[3] != '':
-            enabled = False
-        elif enabled:
-            result += int(match[0]) * int(match[1])
+            if value == "A":
+                if(checkCross(word_search, x, y)):
+                    result += 1
 
     print(result)
+
+def checkCross(map, x, y):
+    
+    if x < 1 or x >= len(map[0]) - 1:
+        return False
+    
+    if y < 1 or y >= len(map) - 1:
+        return False
+    
+    corners = [
+        map[y-1][x-1],
+        map[y+1][x-1],
+        map[y-1][x+1],
+        map[y+1][x+1]
+    ]
+    
+    return corners.count("M") == 2 \
+        and corners.count("S") == 2 \
+        and corners[0] != corners[3]
 
 if __name__ == "__main__":
     part1()

7bea1fbe1f2e05c1d7f5fb9d033af35c06887d54
Henrik Hagfeldt <henke9606@hotmail.com>
2024-12-06 23:33:48 +0100

    Finished day6

--- day6.py
+++ day6.py
@@ -61,86 +61,76 @@ def pos_in_layout(layout, guard_pos):
     return guard_pos[0] >= 0 and guard_pos[0] < len(layout[0]) and guard_pos[1] >= 0 and guard_pos[1] < len(layout)
 
 def part2():
-    layout = []
-    guard_pos = None
-    y = 0
 
-    guard_direction = (0, -1)
+    layout = []
+    initial_guard_pos = None
+    initial_guard_dir = (0, -1)
+    y = 0
 
     with open(getPath(6, example=False), "r") as f:
         for line in f.readlines():
             if '^' in line:
-                guard_pos = (line.index('^'), y) 
+                initial_guard_pos = (line.index('^'), y) 
             layout.append(line.strip())
             y += 1
+    
+    guard_pos = initial_guard_pos
+    guard_direction = initial_guard_dir
 
-    print(layout)
-    print(guard_pos)
-
-    turns = 0
-    steps_taken = 0
-    steps_loop = []
-    loopies = 0
-
-    current_obstacle = None
-    start_guard_pos = None
-    start_guard_direction = None
-
-    previous = []
-    previous_directions = []
-
+    o_guard_traversed = [guard_pos]
+    
     while True:
+        
         # Move guard
         next_pos = get_next_pos(layout, guard_pos, guard_direction)
-        previous.append((next_pos, guard_direction))
-
-        print(f"Next: {next_pos}, Start Guard: {start_guard_pos} {start_guard_direction}, Obstacle: {current_obstacle}")
-
         if (next_pos == None):
-            if (current_obstacle == None):
-                break
-            else:
-                clear(layout, current_obstacle)
-                current_obstacle = None
-                guard_pos = start_guard_pos
-                guard_direction = start_guard_direction
-                previous = []
-                continue
+            break
 
         next_value = layout[next_pos[1]][next_pos[0]]
 
-        # Next is a wall
         if (next_value == '#'):
-            if (guard_pos, guard_direction) in previous and current_obstacle != None:
-                print(f"Found loopy - {current_obstacle[0]}, {current_obstacle[1]}")
-                loopies += 1
-                clear(layout, current_obstacle)
-                current_obstacle = None
-            else:
-                guard_direction = get_next_guard_direction(guard_direction)
-                steps_loop.append(steps_taken)
-                steps_taken = 0
-                turns += 1
-
-        # Next is not a wall
+            guard_direction = get_next_guard_direction(guard_direction)
         else:
+            if not next_pos in o_guard_traversed:
+                o_guard_traversed.append(next_pos)
             guard_pos = next_pos
-            steps_taken += 1
 
-            # No obstacle set
-            if current_obstacle == None and turns > 3:
-                start_guard_direction = guard_direction
-                start_guard_pos = guard_pos
-                current_obstacle = next_pos
-                set(layout, current_obstacle)
+    loops = 0
+
+    for pos in o_guard_traversed:
+
+        guard_pos = initial_guard_pos
+        guard_direction = initial_guard_dir
+
+        guard_traversed = {(guard_pos, guard_direction): ""}
         
-    print(loopies)
+        x = pos[0]
+        y = pos[1]
 
-def clear(layout, obstacle):
-    layout[obstacle[1]] = f"{layout[obstacle[1]][:obstacle[0]]}.{layout[obstacle[1]][obstacle[0] + 1:]}"
+        if layout[y][x] == '#':
+            continue
+        
+        while True:
+            # Move guard
+            next_pos = get_next_pos(layout, guard_pos, guard_direction)
 
-def set(layout, obstacle):
-    layout[obstacle[1]] = f"{layout[obstacle[1]][:obstacle[0]]}#{layout[obstacle[1]][obstacle[0] + 1:]}"
+            if (next_pos, guard_direction) in guard_traversed:
+                loops += 1
+                break
+
+            # Guard out of bounds
+            if (next_pos == None):
+                break
+
+            next_value = layout[next_pos[1]][next_pos[0]]
+
+            if (next_pos == pos or next_value == '#'):
+                guard_direction = get_next_guard_direction(guard_direction)
+            else:
+                guard_traversed[((next_pos, guard_direction))] = ""
+                guard_pos = next_pos
+            
+    print(loops)
 
 if __name__ == "__main__":
     part2()
